60f992d69c7c2d058818366b7f934ab5d92456f6
chrisdemee <526cdemetri@frhsd.com>
2025-05-13 09:31:21 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,34 @@
+const players = [
+    {
+        firstName: 'Josh',
+        lastName: 'Allen',
+        position: 'QB',
+        age: 28,
+        photo: 'https://picsum.photos/200'
+    },
+    {
+        firstName: 'Josh',
+        lastName: 'Allen',
+        position: 'QB',
+        age: 28,
+        photo: 'https://picsum.photos/200'
+    }, {
+        firstName: 'Josh',
+        lastName: 'Allen',
+        position: 'QB',
+        age: 28,
+        photo: 'https://picsum.photos/200'
+    }, {
+        firstName: 'Josh',
+        lastName: 'Allen',
+        position: 'QB',
+        age: 28,
+        photo: 'https://picsum.photos/200'
+    }, {
+        firstName: 'Josh',
+        lastName: 'Allen',
+        position: 'QB',
+        age: 28,
+        photo: 'https://picsum.photos/200'
+    },
+]
